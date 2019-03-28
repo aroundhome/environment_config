f75007a2a20ea6e27af39e82918352a5d55bf97c
Dennis van de Hoef <dennis.vandehoef@aroundhome.de>
2019-03-28 14:45:56 +0100

    Move spec, to be an integration spec

--- spec/environment_config_spec.rb
+++ spec/environment_config_spec.rb
@@ -20,6 +20,24 @@ RSpec.describe EnvironmentConfig do
     it { is_expected.to eql 42 }
   end
 
+  context 'when two configurations have been loaded' do
+    before do
+      ENV['TEST_A'] = 'test_a'
+      ENV['TEST_B'] = 'test_b'
+    end
+
+    let!(:config_a) { EnvironmentConfig.load { |c| c.string 'TEST_A' } }
+    let!(:config_b) { EnvironmentConfig.load { |c| c.string 'TEST_B' } }
+
+    it 'config A does not have an accessor for the value of config B' do
+      expect(config_a).not_to respond_to(:b)
+    end
+
+    it 'config B does not have an accessor for the value of config A' do
+      expect(config_b).not_to respond_to(:a)
+    end
+  end
+
   context 'when a boolean is added' do
     before do
       ENV['TEST_BOOLEAN_VALUE'] = 'true'
